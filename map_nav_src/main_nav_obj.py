9dd0b7ff38d20b01aff13ed67ccfd1bfca5c6c5d
FeiyangLiu <341951286lfy@gmail.com>
2026-03-16 10:47:19 +0800

    Update main_nav_obj.py

--- map_nav_src/main_nav_obj.py
+++ map_nav_src/main_nav_obj.py
@@ -20,7 +20,7 @@ from reverie.parser import parse_args
 
 
 def build_dataset(args, rank=0):
-    tok = get_tokenizer(args)
+    _ = get_tokenizer(args)
 
     feat_db = ImageFeaturesDB(args.img_ft_file, args.image_feat_size)
     obj_db = ObjectFeatureDB(args.obj_ft_file, args.obj_feat_size)
@@ -28,16 +28,13 @@ def build_dataset(args, rank=0):
 
     dataset_class = ReverieObjectNavBatch
 
-    # because we don't use distributed sampler here
-    # in order to make different processes deal with different training examples
-    # we need to shuffle the data with different seed in each processes
     if args.aug is not None:
         aug_instr_data = construct_instrs(
             args.anno_dir,
             args.dataset,
             [args.aug],
             tokenizer=args.tokenizer,
-            max_instr_len=args.max_instr_len
+            max_instr_len=args.max_instr_len,
         )
         aug_env = dataset_class(
             feat_db,
@@ -66,7 +63,7 @@ def build_dataset(args, rank=0):
             args.dataset,
             ['train'],
             tokenizer=args.tokenizer,
-            max_instr_len=args.max_instr_len
+            max_instr_len=args.max_instr_len,
         )
         train_env = dataset_class(
             feat_db,
@@ -95,7 +92,7 @@ def build_dataset(args, rank=0):
             args.dataset,
             [split],
             tokenizer=args.tokenizer,
-            max_instr_len=args.max_instr_len
+            max_instr_len=args.max_instr_len,
         )
         val_env = dataset_class(
             feat_db,
@@ -128,24 +125,23 @@ def train(args, train_env, val_envs, aug_env=None, rank=-1):
         record_file = os.path.join(args.log_dir, 'train.txt')
         write_to_record_file(str(args) + '\n\n', record_file)
 
-    agent_class = GMapObjectNavAgent
-    listner = agent_class(args, train_env, rank=rank)
+    listener = GMapObjectNavAgent(args, train_env, rank=rank)
 
     start_iter = 0
     if args.resume_file is not None:
-        start_iter = listner.load(os.path.join(args.resume_file))
+        start_iter = listener.load(os.path.join(args.resume_file))
         if default_gpu:
             write_to_record_file(
-                "\nLOAD the model from {}, iteration ".format(args.resume_file, start_iter),
-                record_file
+                "\nLOAD the model from {}, iteration {}".format(args.resume_file, start_iter),
+                record_file,
             )
 
     if args.eval_first:
         loss_str = "validation before training"
         for env_name, env in val_envs.items():
-            listner.env = env
-            listner.test(use_dropout=False, feedback='argmax', iters=None)
-            preds = listner.get_results()
+            listener.env = env
+            listener.test(use_dropout=False, feedback='argmax', iters=None)
+            preds = listener.get_results()
             preds = merge_dist_results(all_gather(preds))
 
             if default_gpu:
@@ -161,97 +157,93 @@ def train(args, train_env, val_envs, aug_env=None, rank=-1):
     if default_gpu:
         write_to_record_file(
             '\nListener training starts, start iteration: %s' % str(start_iter),
-            record_file
+            record_file,
         )
 
-    best_val = {'val_unseen': {"spl": 0., "sr": 0., "state": ""}}
+    best_val = {'val_unseen': {"spl": 0.0, "sr": 0.0, "state": ""}}
 
     for idx in range(start_iter, start_iter + args.iters, args.log_every):
-        listner.logs = defaultdict(list)
+        listener.logs = defaultdict(list)
         interval = min(args.log_every, args.iters - idx)
         iter_ = idx + interval
 
         if aug_env is None:
-            listner.env = train_env
-            listner.train(interval, feedback=args.feedback)
+            listener.env = train_env
+            listener.train(interval, feedback=args.feedback)
         else:
             jdx_length = len(range(interval // 2))
             for jdx in range(interval // 2):
-                listner.env = train_env
-                listner.train(1, feedback=args.feedback)
+                listener.env = train_env
+                listener.train(1, feedback=args.feedback)
 
-                listner.env = aug_env
-                listner.train(1, feedback=args.feedback)
+                listener.env = aug_env
+                listener.train(1, feedback=args.feedback)
 
                 if default_gpu:
                     print_progress(jdx, jdx_length, prefix='Progress:', suffix='Complete', bar_length=50)
 
         if default_gpu:
-            total = max(sum(listner.logs['total']), 1)
-            length = max(len(listner.logs['critic_loss']), 1)
-            critic_loss = sum(listner.logs['critic_loss']) / total
-            policy_loss = sum(listner.logs['policy_loss']) / total
-            OG_loss = sum(listner.logs['OG_loss']) / max(len(listner.logs['OG_loss']), 1)
-            IL_loss = sum(listner.logs['IL_loss']) / max(len(listner.logs['IL_loss']), 1)
-            entropy = sum(listner.logs['entropy']) / total
+            total = max(sum(listener.logs['total']), 1)
+            length = max(len(listener.logs['critic_loss']), 1)
+            critic_loss = sum(listener.logs['critic_loss']) / total
+            policy_loss = sum(listener.logs['policy_loss']) / total
+            og_loss = sum(listener.logs['OG_loss']) / max(len(listener.logs['OG_loss']), 1)
+            il_loss = sum(listener.logs['IL_loss']) / max(len(listener.logs['IL_loss']), 1)
+            entropy = sum(listener.logs['entropy']) / total
 
             writer.add_scalar("loss/critic", critic_loss, idx)
             writer.add_scalar("policy_entropy", entropy, idx)
-            writer.add_scalar("loss/OG_loss", OG_loss, idx)
-            writer.add_scalar("loss/IL_loss", IL_loss, idx)
+            writer.add_scalar("loss/OG_loss", og_loss, idx)
+            writer.add_scalar("loss/IL_loss", il_loss, idx)
             writer.add_scalar("total_actions", total, idx)
             writer.add_scalar("max_length", length, idx)
 
             write_to_record_file(
                 "\ntotal_actions %d, max_length %d, entropy %.4f, IL_loss %.4f, "
                 "OG_loss %.4f, policy_loss %.4f, critic_loss %.4f" % (
-                    total, length, entropy, IL_loss, OG_loss, policy_loss, critic_loss
+                    total, length, entropy, il_loss, og_loss, policy_loss, critic_loss
                 ),
-                record_file
+                record_file,
             )
 
             loss_str = "iter {}".format(iter_)
             for env_name, env in val_envs.items():
-                listner.env = env
-                listner.test(use_dropout=False, feedback='argmax', iters=None)
-                preds = listner.get_results()
+                listener.env = env
+                listener.test(use_dropout=False, feedback='argmax', iters=None)
+                preds = listener.get_results()
                 preds = merge_dist_results(all_gather(preds))
 
-                if default_gpu:
-                    score_summary, _ = env.eval_metrics(preds)
-                    loss_str += ", %s " % env_name
-                    for metric, val in score_summary.items():
-                        loss_str += ', %s: %.2f' % (metric, val)
-                        writer.add_scalar('%s/%s' % (metric, env_name), score_summary[metric], idx)
+                score_summary, _ = env.eval_metrics(preds)
+                loss_str += ", %s " % env_name
+                for metric, val in score_summary.items():
+                    loss_str += ', %s: %.2f' % (metric, val)
+                    writer.add_scalar('%s/%s' % (metric, env_name), score_summary[metric], idx)
 
-                    if env_name in best_val:
-                        if score_summary['spl'] >= best_val[env_name]['spl']:
-                            best_val[env_name]['spl'] = score_summary['spl']
-                            best_val[env_name]['sr'] = score_summary['sr']
-                            best_val[env_name]['state'] = 'Iter %d %s' % (iter_, loss_str)
-                            listner.save(idx, os.path.join(args.ckpt_dir, "best_%s" % env_name))
+                if env_name in best_val and score_summary['spl'] >= best_val[env_name]['spl']:
+                    best_val[env_name]['spl'] = score_summary['spl']
+                    best_val[env_name]['sr'] = score_summary['sr']
+                    best_val[env_name]['state'] = 'Iter %d %s' % (iter_, loss_str)
+                    listener.save(idx, os.path.join(args.ckpt_dir, "best_%s" % env_name))
 
-            if default_gpu:
-                listner.save(idx, os.path.join(args.ckpt_dir, "latest_dict"))
-                write_to_record_file(
-                    '%s (%d %d%%) %s' % (
-                        timeSince(start, float(iter_) / args.iters),
-                        iter_,
-                        float(iter_) / args.iters * 100,
-                        loss_str
-                    ),
-                    record_file
-                )
-                write_to_record_file("BEST RESULT TILL NOW", record_file)
-                for env_name in best_val:
-                    write_to_record_file(env_name + ' | ' + best_val[env_name]['state'], record_file)
+            listener.save(idx, os.path.join(args.ckpt_dir, "latest_dict"))
+            write_to_record_file(
+                '%s (%d %d%%) %s' % (
+                    timeSince(start, float(iter_) / args.iters),
+                    iter_,
+                    float(iter_) / args.iters * 100,
+                    loss_str,
+                ),
+                record_file,
+            )
+            write_to_record_file("BEST RESULT TILL NOW", record_file)
+            for env_name in best_val:
+                write_to_record_file(env_name + ' | ' + best_val[env_name]['state'], record_file)
 
 
 def valid(args, train_env, val_envs, rank=-1):
     default_gpu = is_default_gpu(args)
 
-    agent_class = GMapObjectNavAgent
-    agent = agent_class(args, train_env, rank=rank)
+    agent = GMapObjectNavAgent(args, train_env, rank=rank)
 
     if args.resume_file is not None:
         print("Loaded the listener model at iter %d from %s" % (
@@ -266,24 +258,21 @@ def valid(args, train_env, val_envs, rank=-1):
         write_to_record_file(str(args) + '\n\n', record_file)
 
     for env_name, env in val_envs.items():
-        prefix = 'submit' if args.detailed_output is False else 'detail'
-        output_file = os.path.join(
-            args.pred_dir, "%s_%s_%s.json" % (prefix, env_name, args.fusion)
-        )
+        prefix = 'submit' if not args.detailed_output else 'detail'
+        output_file = os.path.join(args.pred_dir, "%s_%s_%s.json" % (prefix, env_name, args.fusion))
         if os.path.exists(output_file):
             continue
 
         agent.logs = defaultdict(list)
         agent.env = env
-        iters = None
 
         start_time = time.time()
         agent.test(
             use_dropout=False,
             feedback='argmax',
-            iters=iters,
+            iters=None,
             TTA_lr=args.TTA_lr,
-            output_dir=args.output_dir
+            output_dir=args.output_dir,
         )
         print(env_name, 'cost time: %.2fs' % (time.time() - start_time))
 
@@ -299,13 +288,8 @@ def valid(args, train_env, val_envs, rank=-1):
                 write_to_record_file(loss_str + '\n', record_file)
 
             if args.submit:
-                json.dump(
-                    preds,
-                    open(output_file, 'w'),
-                    sort_keys=True,
-                    indent=4,
-                    separators=(',', ': ')
-                )
+                with open(output_file, 'w') as f:
+                    json.dump(preds, f, sort_keys=True, indent=4, separators=(',', ': '))
 
 
 def main():
@@ -319,8 +303,6 @@ def main():
 
     set_random_seed(args.seed + rank)
     train_env, val_envs, aug_env = build_dataset(args, rank=rank)
-
-    global tta_inputs
 
     if not args.test:
         train(args, train_env, val_envs, aug_env=aug_env, rank=rank)
