902d49e5d7cc38a7bcf021b1f13582eb19ae067a
FeiyangLiu <341951286lfy@gmail.com>
2026-03-16 10:48:26 +0800

    Update parser.py

--- map_nav_src/reverie/parser.py
+++ map_nav_src/reverie/parser.py
@@ -1,16 +1,21 @@
 import argparse
-import os
 import math
+import os
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="REVERIE parser for FSTTA + NavTrust-style RGB feature banks")
+    parser = argparse.ArgumentParser(
+        description="REVERIE parser for FSTTA + NavTrust-style RGB feature banks"
+    )
 
     parser.add_argument('--root_dir', type=str, default='../datasets')
     parser.add_argument('--dataset', type=str, default='reverie', choices=['reverie'])
-    parser.add_argument('--output_dir', type=str,
-                        default='../datasets/REVERIE/exprs_map/finetune/dagger-vitbase-seed.12_6',
-                        help='experiment id')
+    parser.add_argument(
+        '--output_dir',
+        type=str,
+        default='../datasets/REVERIE/exprs_map/finetune/dagger-vitbase-seed.12_6',
+        help='experiment id',
+    )
     parser.add_argument('--seed', type=int, default=0)
 
     parser.add_argument('--tokenizer', choices=['bert', 'xlm'], default='bert')
@@ -36,7 +41,7 @@ def parse_args():
     parser.add_argument(
         "--resume_file",
         default='../datasets/REVERIE/exprs_map/finetune/dagger-vitbase-seed.3/ckpts/best_val_unseen_3',
-        help='path of the trained model'
+        help='path of the trained model',
     )
     parser.add_argument("--resume_optimizer", action="store_true", default=False)
 
@@ -48,7 +53,7 @@ def parse_args():
     parser.add_argument(
         '--bert_ckpt_file',
         default='../datasets/REVERIE/exprs_map/pretrain/cmt-vitbase-mlm.mrc.sap.og-init.lxmert-aug.speaker/ckpts/model_step_100000.pt',
-        help='init vlnbert'
+        help='init vlnbert',
     )
 
     parser.add_argument("--ml_weight", type=float, default=0.20)
@@ -56,21 +61,28 @@ def parse_args():
     parser.add_argument("--features", type=str, default='vitbase')
     parser.add_argument('--obj_features', type=str, default='vitbase')
 
-    # NavTrust-style corrupted panorama feature bank
-    parser.add_argument('--rgb_feat_file', type=str, default=None,
-                        help='override args.img_ft_file with a precomputed corrupted panorama hdf5 file')
-    parser.add_argument('--rgb_corrupt_type', type=str, default='none',
-                        choices=[
-                            'none',
-                            'motion_blur',
-                            'low_light',
-                            'low_light_noise',
-                            'spatter',
-                            'flare',
-                            'defocus',
-                            'foreign_object',
-                            'black_out',
-                        ])
+    parser.add_argument(
+        '--rgb_feat_file',
+        type=str,
+        default=None,
+        help='override args.img_ft_file with a precomputed corrupted panorama hdf5 file',
+    )
+    parser.add_argument(
+        '--rgb_corrupt_type',
+        type=str,
+        default='none',
+        choices=[
+            'none',
+            'motion_blur',
+            'low_light',
+            'low_light_noise',
+            'spatter',
+            'flare',
+            'defocus',
+            'foreign_object',
+            'black_out',
+        ],
+    )
     parser.add_argument('--rgb_corrupt_severity', type=float, default=0.0)
 
     parser.add_argument('--fix_lang_embedding', action='store_true', default=False)
@@ -90,13 +102,16 @@ def parse_args():
     parser.add_argument('--no_backtrack', action='store_true', default=False)
     parser.add_argument('--detailed_output', action='store_true', default=True)
 
-    parser.add_argument('--optim', type=str, default='adamW',
-                        choices=['rms', 'adam', 'adamW', 'sgd'])
+    parser.add_argument('--optim', type=str, default='adamW', choices=['rms', 'adam', 'adamW', 'sgd'])
     parser.add_argument('--lr', type=float, default=0.00001, help="the learning rate")
-    parser.add_argument('--decay', dest='weight_decay', type=float, default=0.)
-    parser.add_argument('--feedback', type=str, default='sample',
-                        help='How to choose next position, one of teacher, sample and argmax')
-    parser.add_argument('--epsilon', type=float, default=0.1, help='')
+    parser.add_argument('--decay', dest='weight_decay', type=float, default=0.0)
+    parser.add_argument(
+        '--feedback',
+        type=str,
+        default='sample',
+        help='How to choose next position, one of teacher, sample and argmax',
+    )
+    parser.add_argument('--epsilon', type=float, default=0.1)
 
     parser.add_argument("--angle_feat_size", type=int, default=4)
     parser.add_argument('--image_feat_size', type=int, default=768)
@@ -109,7 +124,6 @@ def parse_args():
 
     parser.add_argument('--train_alg', choices=['imitation', 'dagger'], default='dagger')
 
-    # legacy FSTTA args kept for compatibility
     parser.add_argument('--method', default='sar', type=str, help='no_adapt, tent, eata, sar')
     parser.add_argument('--model', default='vitbase_timm', type=str,
                         help='resnet50_gn_timm or resnet50_bn_torch or vitbase_timm')
@@ -121,31 +135,26 @@ def parse_args():
     parser.add_argument('--TTA_lr', default=0.064 / 64, type=float, help='TTA learning rate')
 
     args, _ = parser.parse_known_args()
-    args = postprocess_args(args)
-    return args
+    return postprocess_args(args)
 
 
 def postprocess_args(args):
-    ROOTDIR = args.root_dir
+    rootdir = args.root_dir
 
     ft_file_map = {
         'vitbase': 'pth_vit_base_patch16_224_imagenet.hdf5',
     }
-    default_img_ft_file = os.path.join(ROOTDIR, 'R2R', 'features', ft_file_map[args.features])
-
-    if args.rgb_feat_file is not None:
-        args.img_ft_file = args.rgb_feat_file
-    else:
-        args.img_ft_file = default_img_ft_file
+    default_img_ft_file = os.path.join(rootdir, 'R2R', 'features', ft_file_map[args.features])
+    args.img_ft_file = args.rgb_feat_file if args.rgb_feat_file is not None else default_img_ft_file
 
     obj_ft_file_map = {
         'vitbase': 'obj.avg.top3.min80_vit_base_patch16_224_imagenet.hdf5',
     }
-    args.obj_ft_file = os.path.join(ROOTDIR, 'REVERIE', 'features', obj_ft_file_map[args.obj_features])
+    args.obj_ft_file = os.path.join(rootdir, 'REVERIE', 'features', obj_ft_file_map[args.obj_features])
 
-    args.connectivity_dir = os.path.join(ROOTDIR, 'R2R', 'connectivity')
-    args.scan_data_dir = os.path.join(ROOTDIR, 'Matterport3D', 'v1_unzip_scans')
-    args.anno_dir = os.path.join(ROOTDIR, 'REVERIE', 'annotations')
+    args.connectivity_dir = os.path.join(rootdir, 'R2R', 'connectivity')
+    args.scan_data_dir = os.path.join(rootdir, 'Matterport3D', 'v1_unzip_scans')
+    args.anno_dir = os.path.join(rootdir, 'REVERIE', 'annotations')
 
     args.ckpt_dir = os.path.join(args.output_dir, 'ckpts')
     args.log_dir = os.path.join(args.output_dir, 'logs')
